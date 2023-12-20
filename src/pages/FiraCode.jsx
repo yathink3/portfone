9e226bc1d347f822fe67b4eac5b385db779458d9
yathink3 <yathink3@gmail.com>
2023-12-20 12:02:08 +0530

    (feat)ui : fix changes

--- src/pages/FiraCode.jsx
+++ src/pages/FiraCode.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 
-const Contact = () => {
+const FiraCode = () => {
   return (
-    <section className='relative flex lg:flex-row flex-col max-container'>
-      <Link to={'/FiraCode.ttf'} target='_blank' className='neo-brutalism-white neo-btn'>
-        Download Firacode
-      </Link>
+    <section className='max-container'>
+      <h3 className='head-text'>
+        Here is the <span className='blue-gradient_text font-semibold drop-shadow'>Firacode</span>
+      </h3>
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <Link to={'/FiraCode.ttf'} target='_blank' className='neo-brutalism-white neo-btn'>
+          Download Firacode
+        </Link>
+      </div>
     </section>
   );
 };
 
-export default Contact;
+export default FiraCode;
