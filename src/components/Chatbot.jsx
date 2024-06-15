0d2dd0aa905be73fe3a9c1b31189450c0a99f36f
yathink3 <yathink3@gmail.com>
2024-06-15 18:15:37 +0530

    (feat)ui : chat bot changes

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -9,7 +9,7 @@ const Chatbot = () => {
 
   return (
     <div>
-      <button className='fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 border rounded-full w-16 h-16 bg-[#5a4ef6] hover:bg-[#6c5cf4] m-0 cursor-pointer border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900' type='button' aria-haspopup='dialog' aria-expanded={isChatOpen} onClick={toggleChat}>
+      <button className='fixed bottom-4 right-4 inline-flex items-center justify-center text-sm font-medium disabled:pointer-events-none disabled:opacity-50 rounded-full w-16 h-16 bg-[#5a4ef6] hover:bg-[#6c5cf4] m-0 cursor-pointer border border-gray-200 bg-none p-0 normal-case leading-5 hover:text-gray-900' type='button' aria-haspopup='dialog' aria-expanded={isChatOpen} onClick={toggleChat}>
         <svg xmlns='http://www.w3.org/2000/svg' width='30' height='40' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='text-white block border-gray-200 align-middle'>
           <path d='m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z' className='border-gray-200'></path>
         </svg>
@@ -27,7 +27,7 @@ const Chatbot = () => {
           {/* Heading */}
           <div className='flex flex-col space-y-1.5 pb-6'>
             <h2 className='font-semibold text-lg tracking-tight'>Chatbot</h2>
-            <p className='text-sm text-[#6b7280] leading-3'>Ask me anything; I am here to help you.</p>
+            <p className='text-sm text-[#6b7280] leading-3 '>Ask me anything; I am here to help you.</p>
           </div>
 
           {/* Chat Container */}
@@ -45,12 +45,17 @@ const Chatbot = () => {
                 </div>
               </span>
               <p className='leading-relaxed'>
-                <span className='block font-bold text-gray-700'>AI </span>Hi, how can I help you today?
+                <span className='block font-bold text-gray-700'>AI </span>
+                <div className='border border-gray-200 px-2 py-1 rounded-2xl rounded-tl-none bg-gray-100'>Hi, how can I help you today?</div>
               </p>
             </div>
 
             {/* User Chat Message */}
-            <div className='flex gap-3 my-4 text-gray-600 text-sm flex-1'>
+            <div className='flex justify-end gap-3 my-4 text-gray-600 text-sm flex-1'>
+              <p className='leading-relaxed'>
+                <span className='flex justify-end block font-bold text-gray-700'>You </span>
+                <div className='border border-gray-200 px-2 py-1 rounded-2xl rounded-tr-none bg-gray-100'>fewafef</div>
+              </p>
               <span className='relative flex shrink-0 overflow-hidden rounded-full w-8 h-8'>
                 <div className='rounded-full bg-gray-100 border p-1'>
                   <svg stroke='none' fill='black' strokeWidth='0' viewBox='0 0 16 16' height='20' width='20' xmlns='http://www.w3.org/2000/svg'>
@@ -58,9 +63,6 @@ const Chatbot = () => {
                   </svg>
                 </div>
               </span>
-              <p className='leading-relaxed'>
-                <span className='block font-bold text-gray-700'>You </span>fewafef
-              </p>
             </div>
 
             {/* AI Chat Message */}
@@ -76,7 +78,8 @@ const Chatbot = () => {
                 </div>
               </span>
               <p className='leading-relaxed'>
-                <span className='block font-bold text-gray-700'>AI </span>Sorry, I couldn't find any information in the documentation about that. Expect answer to be less accurateI could not find the answer to this in the verified sources.
+                <span className='block font-bold text-gray-700'>AI </span>
+                <div className='border border-gray-200 px-2 py-1 rounded-2xl rounded-tl-none bg-gray-100'>Sorry, I couldn't find any information in the documentation about that. Expect answer to be less accurateI could not find the answer to this in the verified sources.</div>
               </p>
             </div>
           </div>
